1e2e5a31c9a5dc514738c9f1e77604fb0a37222b
SilvioBruno <sbnascimento8@gmail.com>
2024-06-21 16:47:13 -0300

    testes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-var nome = "silvio"
+var Xp = parseInt(prompt("Digite seu Xp"))
+var Name = prompt("Digite o nome do jogador")
+var Level = ""
+if (Xp < 1000) {
+    Level = "Ferro"
+}
+else if (Xp > 1000 && Xp < 2001) {
+    Level = "Bronze"
+}
+else if (Xp > 2000 && Xp < 5001) {
+    Level = "Prata"
+}
 
-console.log(nome)
+console.log("O Herói de nome:" + nome + "está no nível de " + Level)
