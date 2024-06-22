592db3b32022987a4ca6792f58fcf401ee26cef7
SilvioBruno <sbnascimento8@gmail.com>
2024-06-22 09:02:15 -0300

    Verificador de Xp Desafio Godot Santander

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
-var Xp = parseInt(prompt("Digite seu Xp"))
-var Name = prompt("Digite o nome do jogador")
+const prompt = require('prompt-sync')();
+
+var N = parseInt(prompt("Insira o número de players que queira saber o Xp "))
 var Level = ""
-if (Xp < 1000) {
-    Level = "Ferro"
-}
-else if (Xp > 1000 && Xp < 2001) {
-    Level = "Bronze"
-}
-else if (Xp > 2000 && Xp < 5001) {
-    Level = "Prata"
+
+    for (var i = 0 ; i < N ; i++) {
+        var Xp = parseInt(prompt("Digite seu Xp: "))
+        var Name = prompt("Digite o nome do jogador: ")
+
+    if (Xp < 1000) {
+        Level = "Ferro"
+    }
+    else if (Xp > 1000 && Xp < 2001) {
+        Level = "Bronze"
+    }
+    else if (Xp > 2000 && Xp < 5001) {
+        Level = "Prata"
+    }
+    else if (Xp > 5000 && Xp < 7001) {
+        Level = "Ouro"
+    }
+    else if (Xp > 7000 && Xp < 8001) {
+        Level = "Platina"
+    }
+    else if (Xp > 8000 && Xp < 9001){
+        Level = "Ascendente"
+    }
+    else if (Xp > 9000 && Xp < 10001) {
+        Level = "Imortal"
+    }
+    else{
+        Level = "Radiante"
+    }
+    
+        console.log("O Herói de nome: " + Name + " está no nível de " + Level)
 }
 
-console.log("O Herói de nome:" + nome + "está no nível de " + Level)
+
+
+
